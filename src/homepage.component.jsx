7d619744a78c23172320702eea028e2171da9901
Leo <lcheung@swellbottle.com>
2022-02-19 08:26:03 -0500

    add node package and homepage styles

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './homepage.styles.scss'
 
 const HomePage = () => (
   <div className="homepage">
@@ -12,6 +13,12 @@ const HomePage = () => (
       <div className="menu-item">
         <div className="content">
           <h1 className="title">JACKETS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
