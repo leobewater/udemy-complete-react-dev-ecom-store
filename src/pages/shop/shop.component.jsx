7f302a036b25bcb434cf7870024742c8299e7172
Leo <lcheung@swellbottle.com>
2022-02-20 10:16:40 -0500

    populate items to shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
 
 import SHOP_DATA from './shop.data'
 
@@ -12,7 +13,14 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return <div>Shop Page</div>
+    const { collections } = this.state
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    )
   }
 }
 
