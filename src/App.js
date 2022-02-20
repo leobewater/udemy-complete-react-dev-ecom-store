33a092805f09c2ea36797f9a1575c6406769e754
Leo <lcheung@swellbottle.com>
2022-02-20 15:35:55 -0500

    add sign in page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import './App.css'
+
 import Header from './components/header/header.component'
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   )
