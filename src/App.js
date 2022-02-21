c03233659638baaf90a152fbd0ea872f537a1923
Leo <lcheung@swellbottle.com>
2022-02-21 08:22:49 -0500

    convert App to a class and fetch logged in user when mount

--- src/App.js
+++ src/App.js
@@ -7,18 +7,38 @@ import Header from './components/header/header.component'
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { auth } from './firebase/firebase.utils'
+// import { render } from 'node-sass'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/shop" element={<ShopPage />} />
-        <Route path="/signin" element={<SignInAndSignUpPage />} />
-      </Routes>
-    </div>
-  )
+// convert App to a Stateful class with React component
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+      console.log(user)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/signin" element={<SignInAndSignUpPage />} />
+        </Routes>
+      </div>
+    )
+  }
 }
 
 export default App
